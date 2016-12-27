c5cd467c300d2d588f4d02a4b2e75e9d9641cfe1
Jérôme Fenal <jfenal@free.fr>
2016-12-27 16:01:33 +0100

    Add test-04

--- test/packaging/os/redhat_repositories/test-all.sh
+++ test/packaging/os/redhat_repositories/test-all.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+pushd ${0%/*}
 ansible-playbook -i hosts test-01.yml
 ansible-playbook -i hosts test-02.yml
 ansible-playbook -i hosts test-03.yml
+ansible-playbook -i hosts test-04.yml
+popd
